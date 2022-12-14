4b0acff33b93ac587b9c24d4b895c2ef046509ff
kyawoo-23 <kyawkyawjek@gmail.com>
2022-12-14 19:48:20 +0630

    initial commit

---
+++ assets/app.js
@@ -0,0 +1,45 @@
+let paths = document.querySelectorAll('path');
+let svgImg = document.querySelector('.svg-img');
+let colorImg = document.querySelector('.color-img');
+let mainTitle = document.querySelector('.main-title');
+
+scrollAnimation()
+document.addEventListener('scroll', scrollAnimation)
+
+function scrollAnimation() {
+
+    // calculation method when scrolling within an element
+    // let height = scroller.clientHeight;
+    // let scrollHeight = scroller.scrollHeight - height;
+    // let scrollTop = scroller.scrollTop;
+    // let percent = Math.floor(scrollTop / scrollHeight * 100);
+
+    let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+
+    // fill svg paths
+    for (var i = 0; i < paths.length; i++) {
+        let path = paths[i];
+        let pathLength = path.getTotalLength();
+        path.style.strokeDasharray = pathLength;
+        path.style.strokeDashoffset = pathLength;
+        let drawLength = pathLength * scrollPercentage;
+        path.style.strokeDashoffset = pathLength - drawLength;
+    }
+
+    switch (scrollPercentage) {
+        case 0:
+            mainTitle.style.opacity = 1;
+        case 1:
+            svgImg.style.opacity = 0;
+            colorImg.style.opacity = 1;
+            colorImg.style.transform = "scale(1)";
+            break;
+        default:
+            mainTitle.style.opacity = 0;
+            svgImg.style.opacity = 1;
+            colorImg.style.opacity = 0;
+            colorImg.style.transform = "scale(.9)";
+            break;
+    }
+
+}
